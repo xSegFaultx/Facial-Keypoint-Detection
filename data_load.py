203786c945c4e2cd30dd5f0df0cab99ec675e67a
Ran Sa <leodre1022@hotmail.com>
2022-07-11 03:09:17 -0500

    Update code to work with newer version of pandas and matplotlib

--- data_load.py
+++ data_load.py
@@ -1,9 +1,7 @@
-import glob
 import os
 import torch
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
-import matplotlib.image as mpimg
 import pandas as pd
 import cv2
 
@@ -28,15 +26,16 @@ class FacialKeypointsDataset(Dataset):
 
     def __getitem__(self, idx):
         image_name = os.path.join(self.root_dir,
-                                self.key_pts_frame.iloc[idx, 0])
-        
-        image = mpimg.imread(image_name)
-        
+                                  self.key_pts_frame.iloc[idx, 0])
+
+        image = cv2.imread(image_name)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
         # if image has an alpha color channel, get rid of it
-        if(image.shape[2] == 4):
-            image = image[:,:,0:3]
-        
-        key_pts = self.key_pts_frame.iloc[idx, 1:].as_matrix()
+        if (image.shape[2] == 4):
+            image = image[:, :, 0:3]
+
+        key_pts = self.key_pts_frame.iloc[idx, 1:].to_numpy()
         key_pts = key_pts.astype('float').reshape(-1, 2)
         sample = {'image': image, 'keypoints': key_pts}
 
@@ -44,31 +43,28 @@ class FacialKeypointsDataset(Dataset):
             sample = self.transform(sample)
 
         return sample
-    
 
-    
-# tranforms
+
+# transforms
 
 class Normalize(object):
-    """Convert a color image to grayscale and normalize the color range to [0,1]."""        
+    """Convert a color image to grayscale and normalize the color range to [0,1]."""
 
     def __call__(self, sample):
         image, key_pts = sample['image'], sample['keypoints']
-        
+
         image_copy = np.copy(image)
         key_pts_copy = np.copy(key_pts)
 
         # convert image to grayscale
         image_copy = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        
+
         # scale color range from [0, 255] to [0, 1]
-        image_copy=  image_copy/255.0
-            
-        
+        image_copy = image_copy / 255.0
+
         # scale keypoints to be centered around 0 with a range of [-1, 1]
         # mean = 100, sqrt = 50, so, pts should be (pts - 100)/50
-        key_pts_copy = (key_pts_copy - 100)/50.0
-
+        key_pts_copy = (key_pts_copy - 100) / 50.0
 
         return {'image': image_copy, 'keypoints': key_pts_copy}
 
@@ -101,7 +97,7 @@ class Rescale(object):
         new_h, new_w = int(new_h), int(new_w)
 
         img = cv2.resize(image, (new_w, new_h))
-        
+
         # scale the pts, too
         key_pts = key_pts * [new_w / w, new_h / h]
 
@@ -134,7 +130,7 @@ class RandomCrop(object):
         left = np.random.randint(0, w - new_w)
 
         image = image[top: top + new_h,
-                      left: left + new_w]
+                left: left + new_w]
 
         key_pts = key_pts - [left, top]
 
@@ -146,16 +142,16 @@ class ToTensor(object):
 
     def __call__(self, sample):
         image, key_pts = sample['image'], sample['keypoints']
-         
+
         # if image has no grayscale color channel, add one
-        if(len(image.shape) == 2):
+        if (len(image.shape) == 2):
             # add that third color dim
             image = image.reshape(image.shape[0], image.shape[1], 1)
-            
+
         # swap color axis because
         # numpy image: H x W x C
         # torch image: C X H X W
         image = image.transpose((2, 0, 1))
-        
+
         return {'image': torch.from_numpy(image),
                 'keypoints': torch.from_numpy(key_pts)}
